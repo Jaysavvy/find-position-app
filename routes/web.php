71d140b3f7ea6ea49c67ec2e970b1cda91677210
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2024-07-12 11:24:14 -0400

    created sign up and login features

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 
 Route::get('/', [App\Http\Controllers\JobController::class, 'index']);
+
+Route::middleware('guest')->group(function () {
+
+    Route::get('/register', [RegisteredUserController::class, 'create']);
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+    Route::get('/login', [SessionController::class, 'create']);
+    Route::post('/login', [SessionController::class, 'store']);
+});
+
+
+
+Route::post('/logout', [SessionController::class, 'destroy'])->middleware('auth');
